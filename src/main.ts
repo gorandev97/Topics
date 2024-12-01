118fcc70f1f25e958ae7a66956975ab16b9dfdbb
Goran Gotovac <goran.gotovac97@gmail.com>
2024-12-01 19:25:22 +0100

    Changed env variables name

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTED_URL,
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
